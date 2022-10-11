4af251063158fe83103a36dc2a26f0caaf021cb5
JayChandlani <jaychandlanijay@gmail.com>
2022-10-11 10:13:44 +0530

    added day3 files

---
+++ Introduction_to_Javascript/Day03/pb03.js
@@ -0,0 +1,10 @@
+let a = 1;
+let b=2;
+
+if(a>b){
+  console.log(a,"is greater")
+}else if(b>a){
+  console.log(b,"is greater")
+}else {
+  console.log("Both are equal")
+}
---
+++ Introduction_to_Javascript/Day03/pb04.js
@@ -0,0 +1,8 @@
+let userName = "Masai";
+let password = "Masai001";
+
+if(userName === "Masai"&& password === "Masai001"){
+  console.log("User can login")
+}else{
+  console.log("user can not login");
+}
