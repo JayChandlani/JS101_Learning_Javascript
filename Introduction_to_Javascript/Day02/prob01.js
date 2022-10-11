8b9236d2028ae0932dc724662401ff690edd6d84
JayChandlani <jaychandlanijay@gmail.com>
2022-10-11 10:10:26 +0530

    added day2 files

---
+++ Introduction_to_Javascript/Day02/prob01.js
@@ -0,0 +1,10 @@
+let birth = 2009;
+
+let year = 2022;
+
+if(year-birth<=19&&year-birth>=13){
+  console.log("Teenage");
+}else if(year-birth<=29&&year-birth>=20){
+  console.log("Twenties");
+}
+
---
+++ Introduction_to_Javascript/Day02/prob02.js
@@ -0,0 +1,7 @@
+let character = "a";
+
+if(character==="a"||character==="A"||character==="e"||character==="E"||character==="i"||character==="I"||character==="o"||character==="O"||character==="u"||character==="U"){
+  console.log("Vowel");
+}else{
+  console.log("Consonant");
+}
