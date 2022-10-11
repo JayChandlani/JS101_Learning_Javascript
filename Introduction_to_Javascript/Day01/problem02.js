049b65d88ea34a5f2fc1ed9b6d2a821f83912a3b
JayChandlani <jaychandlanijay@gmail.com>
2022-10-11 09:59:57 +0530

    added day01 files

---
+++ Introduction_to_Javascript/Day01/problem02.js
@@ -0,0 +1,6 @@
+let name = "Jay Chandlani";
+console.log(name);
+ name = "Bhagwan Das Chandlani";
+console.log(name);
+ name = "Lata Chandlani";
+console.log(name);
---
+++ Introduction_to_Javascript/Day01/problem04.js
@@ -0,0 +1,12 @@
+let name = "Name-Jay Chandlani"
+let school= "School-Masai School"
+let grade = "Grade-A"
+let section = "Section-A"
+let rollno = "Roll No.21"
+let marks = "Maths-90 English-95 Science-97"
+console.log(name)
+console.log(school)
+console.log(grade)
+console.log(section)
+console.log(rollno)
+console.log(marks)
